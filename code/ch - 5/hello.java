8c51ac65e2293990b62bbecca05938ecd18d95a9
Harsh Sutariya <hr.sutariya1@gmail.com>
2023-04-22 16:51:44 +0530

    some changes

--- code/ch - 5/hello.java
+++
@@ -1,9 +0,0 @@
-public class hello 
-{
-     public static void main(String[] args)
-     {
-          System.out.println("hello, harsh");
-          System.out.print("my name is harsh too.\n");
-          System.out.print("good bye");
-     }
-}
