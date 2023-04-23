867eb439c20a7757cd87ea91d5e6685f0ba97d42
Harsh Sutariya <hr.sutariya1@gmail.com>
2023-04-24 00:56:23 +0530

    updated.

--- Patterns in JAVA/Hollow_Rectangle_pattern.java
+++ Patterns in JAVA/Hollow_Rectangle_pattern.java
@@ -9,9 +9,9 @@ public class Hollow_Rectangle_pattern {
                //inner loop (for columns)
                for(int j = 1; j<=total_column; j++){
                     if(i==1||i==total_row||j==1||j==total_column){
-                         System.out.print("*");
+                         System.out.print("* ");
                     }else{
-                         System.out.print(" ");
+                         System.out.print("  ");
                     }
                }
                System.out.println();
